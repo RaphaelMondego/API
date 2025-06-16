ddff95930ad6a86279e43164e05df4dd844cf22f
raphael.mondego <raphamondego@gmail.com>
2025-06-16 17:37:33 -0300

    pequenas mudanças  - work

--- server.js
+++ server.js
@@ -2,6 +2,14 @@ import express from 'express';
 
 const app = express()
 
+const users = []
+
+app.post('/usuarios', (req, res) => {
+
+    
+
+})
+
 // Create a simple route
 app.get('/usuarios',(req, res) => {
     res.send('Deu green!')
