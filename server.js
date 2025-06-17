996c2c2d1f90d403e1bf591becdbb6a92c9785ef
Raphael Mondego <raphamondego@gmail.com>
2025-06-16 21:42:09 -0300

    criação e conexão do Mongo DB junto ao Prisma

--- server.js
+++ server.js
@@ -1,19 +1,26 @@
 import express from 'express';
 
 const app = express()
+app.use(express.json()) // essa linha garante que o express entenda o corpo da requisição, o body, como JSON
 
 const users = []
 
 app.post('/usuarios', (req, res) => {
 
-    
+    users.push(req.body) // o push adiciona os dados do usuário que enviamos na TC.
 
+    res.status(201).json(req.body) // aqui estamos retornando o usuário que foi criado, com o status 201 (Created)
 })
 
-// Create a simple route
+
 app.get('/usuarios',(req, res) => {
-    res.send('Deu green!')
+    res.status(200).json(users) // aqui estamos retornando todos os usuários que foram criados, com o status 200 (OK)
 }); 
 
 // Start the server, using "listen"
 app.listen(3000)
+
+
+/* 
+ Usuário: raphamondego
+ Senha: NC9OFVK7a7DjuEje */
